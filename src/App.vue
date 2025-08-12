f8c6e6e07866c70ad72a356d57fbb9428678b631
ruoxidamowang <2435169750@qq.com>
2025-08-12 16:28:47 +0800

    页面完善

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app" style="width:100%;height:100%">
+    <router-view /> <!-- 👈 路由出口 -->
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
 
 // 引入 DataV
-import DataV, { setClassNamePrefix } from '@dataview/datav-vue3';
+import DataVVue3 from '@kjgl77/datav-vue3'
 
 const app = createApp(App)
 app.use(router)
-app.use(DataV, { classNamePrefix: 'dv-' });
+app.use(DataVVue3);
 app.mount('#app')
