c3b685c6bf9e10aa6c0b8aedd07fcff98dd0c9f9
ruoxidamowang <2435169750@qq.com>
2025-08-12 16:34:18 +0800

    使用node22版本

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,10 @@
 
 <template>
   <div id="app" style="width:100%;height:100%">
-    <router-view /> <!-- 👈 路由出口 -->
+    <router-view />
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
