5acefb7b026ef6a1b3d37777ec58e423971586ee
ruoxidamowang <2435169750@qq.com>
2025-08-12 15:54:16 +0800

    添加路由和页面组件

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="width:100%;height:100vh;background:#0b1226;">
+    <dv-border-box-1 style="width:100%;height:100%;">
+      <div style="padding:20px;color:#fff;">
+        <h1>🚀 BigScreen Demo</h1>
+        <p>这里可以放 DataV 组件和图表</p>
+      </div>
+    </dv-border-box-1>
+  </div>
+</template>
+
+<script setup>
+
+defineOptions({
+  name: 'Home',
+})
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
